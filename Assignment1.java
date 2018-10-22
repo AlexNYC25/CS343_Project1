693965534a5e98b3d69fd63ee275daeb38d87ff9
Alexis Montes <montes.alexis25@gmail.com>
2018-10-22 15:48:55 -0400

    update

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,17 @@
+/**
+	currently there is a problem where the test each integer has a bug
+	that dosen't read the data from playGame correctly/.
+**/
 
+public class Assignment1{
 
-public Class Assignment1{
-	String playGame(int chosenNumber){
+	public static void main(String[] args){
+		System.out.println("Program starting out.");
+		new Assignment1().testEachInteger();
+
+	}
+
+	private String playGame(int chosenNumber){
 		/**
 			The chosen integer is passed and returns a string of ones and zeros
 		**/
@@ -12,10 +22,10 @@ public Class Assignment1{
 			for (int z = 0; z < 6; z++){
 				middle = (lower + upper) / 2;
 				if (chosenNumber > middle){
-					toBeReturned += "1"
+					toBeReturned += "1";
 				}
 				else{
-					toBeReturned += "0"
+					toBeReturned += "0";
 				}
 				upper = middle;
 			}
@@ -23,7 +33,7 @@ public Class Assignment1{
 		return toBeReturned;
 	}
 
-	public void testEachInteger(){
+	private void testEachInteger(){
 		/**
 			this will play the game for each integer from 1 to 100
 			printing out the result of the game for each integer
@@ -42,10 +52,30 @@ public Class Assignment1{
 				remember average is a weighted average
 
 		**/
+
+		System.out.println("rinning this now");
+
+		int q6 = 0, q7 = 0;
+		for (int x = 0; x <= 1; x++){
+			int currentNumber = x;
+			String currentResult = playGame(currentNumber);
+			if (currentResult == "000000"){
+				q7++;
+			}
+			else{
+				q6++;
+			}
+		}
+
+		int averageWeighted = ((q6 * 6) + (q7 * 7 )) / 1000;
+
+		System.out.println("Total number of integers where 6 questions are asked is " + q6);
+		System.out.println("Total number of integers where 7 questions are asked is " + q7);
+		System.out.println("The wegihted average is " + averageWeighted);
 	}
 
 
-	public void playOneThousandGames(){
+	private void playOneThousandGames(){
 		/**
 			This will play the game 1000 timesm for each game a randomly 
 			chosen integer is chosen from 1 to 100
@@ -53,5 +83,11 @@ public Class Assignment1{
 			Have this ialso print out the in addition conditions from 
 			testEachInteger
 		**/
+
+
 	}
+
+
+
+
 }
